4e57c006d158c9898e85193f845176415f04fd80
Dittemor <eaa25dca@students.eaaa.dk>
2025-12-06 16:35:04 +0100

    Små ændringer + start på produktside

---
+++ product.js
@@ -0,0 +1,18 @@
+// Search input
+
+const searchContainer = document.querySelector('.search-container');
+const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', () => {
+    searchContainer.classList.toggle('active');
+    if (searchContainer.classList.contains('active')) {
+        searchContainer.querySelector('.search-input').focus();
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (!searchContainer.contains(e.target)) {
+        searchContainer.classList.remove('active');
+    }
+}); 
+
